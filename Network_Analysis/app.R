57e00860bff7448f7dad1b7c47bda3c02c61576f
RodrigoZepeda <rzepeda17@gmail.com>
2019-06-17 15:14:16 -0500

    added navbar

--- Network_Analysis/app.R
+++ Network_Analysis/app.R
@@ -12,9 +12,11 @@ server <- function(input, output) {
 
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+        2+8
     })
 
 }
 
-shinyApp(ui = htmlTemplate("www/network.html"), server)
+shinyApp(ui = htmlTemplate("www/network.html",
+                           button = actionButton("action", "Action"),
+                           slider = sliderInput("x", "X", 1, 100, 50)), server)
